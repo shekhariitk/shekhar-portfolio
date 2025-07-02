ca8cc011568a20ee24a0a8258488c6eb4371dd81
Shekhar Suman <s.sumanpathak513@gmail.com>
2025-07-02 18:42:21 +0530

    she

--- app.py
+++ app.py
@@ -703,4 +703,4 @@ def internal_error(error):
     return jsonify({"success": False, "error": "Internal server error"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0', port = 5000)
